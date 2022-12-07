f0507a209dacf008af49aba530d5031b4ccab314
farhankhalid08 <farhan.khalid@hotmail.co.uk>
2022-12-06 22:51:48 -0500

    new

--- src/test/java/test6/Class6.java
+++ src/test/java/test6/Class6.java
@@ -1,36 +1,20 @@
 package test6;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Class6 {
+	public static WebDriver driver;
+	public static void main (String[] args) {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://rediff.com");
+		
+		driver.findElement(By.cssSelector("a.signin")).click();
+		driver.findElement(By.id("login1")).sendKeys("farhankhalid920@hotmail.com");
+	    driver.findElement(By.id("password")).sendKeys("subhan");
+	    driver.quit();
+	}
 
-	
-	@Test(dataProvider = "provider")
-	public void GetData(String username, String paswword,int ID, String browser) {
-		
-	}
-	
-	@DataProvider
-	public Object[][] provider(){
-		Object[][] data = new Object[3][4];
-		
-		data[0][0]="Farhan";
-		data[0][1]="khalid";
-		data[0][2]=123;
-		data[0][3]="chrome";
-		
-		data[1][0]="Farhan1";
-		data[1][1]="khalid1";
-		data[1][2]=1234;
-		data[1][3]="chrome1";
-		
-		data[2][0]="Farhan2";
-		data[2][1]="khalid2";
-		data[2][2]=12345;
-		data[2][3]="chrome2";
-		
-		return data;
-		
-	}
 }
